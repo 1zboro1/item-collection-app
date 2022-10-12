14b1122d549f6eebfbd9d2d50029f2ae480fae72
Bartosz Zborowski <zboroplayshard@gmail.com>
2022-10-12 18:13:19 +0200

    changes before serving frontend on heroku

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3005;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname + "/client", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
