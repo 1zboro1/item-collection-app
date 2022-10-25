95a94ea3a793fb123d404d66469c4deb57a5119e
Bartosz Zborowski <zboroplayshard@gmail.com>
2022-10-25 15:07:31 +0200

    first fix to deploy app

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname + "/client", "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname + "/client", "build")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname + "/client/build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
 }
 
 app.listen(PORT, () => {
